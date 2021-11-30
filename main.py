9b7332ef4697ca0f40628e4b40c87a0786bd8d02
Partha Pattnayak <83297893+Partha41@users.noreply.github.com>
2021-11-30 12:05:50 +0530

    custom alexa bot uploaded

---
+++ main.py
@@ -0,0 +1,61 @@
+import pyttsx3
+import pywhatkit as pywhatkit
+import speech_recognition as sr
+import wikipedia
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'what is ' in command:
+        obj = command.replace('what is', '')
+        info2 = wikipedia.summary(obj, 1)
+        print(info2)
+        talk(info2)
+    else:
+        talk('please say the command again.')
+        return True
+    return False
+
+flag = True
+
+while flag:
+    flag = run_alexa()
+talk('Good bye.')
+
+
+
